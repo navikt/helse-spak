b0253917ff46a9c5f261419b9713aa6b86a1c59b
Stephen Ramthun <stephenramthun@gmail.com>
2019-10-08 15:21:29 +0200

    Update packages

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,18 +9,6 @@ app.use(bodyParser.json());
 app.get('/isAlive', (req, res) => res.send('alive'));
 app.get('/isReady', (req, res) => res.send('ready'));
 
-app.get('/*', (req, res, next) => {
-    if (!req.accepts('html') && /\/api/.test(req.url)) {
-        console.debug(
-            `Received a non-HTML request for '${req.url}', which didn't match a route`
-        );
-        res.sendStatus(404);
-        return;
-    }
-    next();
-});
-
-// At the time of writing this comment, the setup of the static 'routes' has to be done in a particular order.
 app.use('/static', express.static('dist/client'));
 app.use('/*', express.static('dist/client/index.html'));
 app.use('/', express.static('dist/client/'));
